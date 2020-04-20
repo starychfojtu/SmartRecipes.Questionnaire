1760ab63fe87b1b576179c3ab31af7e378a6e1e0
starychfojtu <josef.starychfojtu@gmail.com>
2020-04-20 12:43:54 +0200

    Model added

---
+++ src/Model.fs
@@ -0,0 +1,56 @@
+﻿module Model
+
+open System
+
+type Ingredient = {
+    DisplayLine: string
+    IsInputMatch: bool
+}
+
+type RecipeId = string
+
+type Recipe = {
+    Id: RecipeId
+    Uri: Uri
+    Name: string
+    Ingredients: Ingredient list
+}
+
+type RecommendationMethodId = string
+
+type RecommendationMethod = {
+    Id: RecommendationMethodId
+    Name: string
+    Recommendations: Recipe list
+}
+
+type User = {
+    Name: string
+}
+
+type Interaction = Liked | Disliked
+
+type MousePosition = {
+    X: int
+    Y: int
+}
+
+type RecipePosition = {
+    MethodPositionFromLeft: int
+    RecipePositionInMethod: int
+}
+
+type Metadata = {
+    MousePosition: MousePosition
+    RecipePosition: RecipePosition
+    TimeStamp: DateTime
+}
+
+type InteractionEvent = {
+    Interaction: Interaction
+    MethodId: RecommendationMethodId
+    RecipeId: RecipeId
+    RecipeUri: Uri
+    UserName: string
+    Metadata: Metadata
+}
